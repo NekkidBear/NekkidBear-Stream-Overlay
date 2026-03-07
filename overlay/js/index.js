67f6ac0cca333ea06ceaf33502e4c61c15a94cef
Jason King-Lowe <NekkidBearGaming@gmail.com>
2026-03-06 19:21:21 -0600

    feat: allow disabling background via URL param

--- overlay/js/index.js
+++ overlay/js/index.js
@@ -8,7 +8,13 @@ import { initTicker } from './ticker.js';
 import { initBackground } from './background.js';
 
 // initialize background first so it sits under everything else
-initBackground();
+// allow disabling via ?nobg=1 on the URL (useful when running chrome.html as a
+// standalone OBS browser source that already has a separate background)
+if (!new URL(window.location.href).searchParams.has('nobg')) {
+  initBackground();
+} else {
+  console.log('overlay: background init skipped (nobg flag)');
+}
 
 // kick off the routines that previously executed in the inline script
 startColorCycle();
