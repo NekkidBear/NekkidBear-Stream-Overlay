3e0b0254aa3aaf97302da8131f4e3b4f88f2f979
Jason King-Lowe <NekkidBearGaming@gmail.com>
2026-03-06 19:16:20 -0600

    feat: embed background animation into overlay

--- overlay/js/index.js
+++ overlay/js/index.js
@@ -5,6 +5,10 @@ import { connectOBS } from './obs.js';
 import { connectRelay } from './relay.js';
 import { initMilestone } from './milestone.js';
 import { initTicker } from './ticker.js';
+import { initBackground } from './background.js';
+
+// initialize background first so it sits under everything else
+initBackground();
 
 // kick off the routines that previously executed in the inline script
 startColorCycle();
